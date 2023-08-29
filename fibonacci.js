6707ca09026c15fb87b6c45f9a2edc083f2e09d5
norphel <norphel317@gmail.com>
2023-08-29 13:26:06 +0530

    iterative version of fibonacci sequence

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+function fibs(n) {
+  let arr = [];
+  arr[0] = 0;
+  arr[1] = 1;
+  for (let i = 2; i < n; i++) {
+    arr[i] = arr[i - 2] + arr[i - 1];
+  }
+  return arr;
+}
+console.log(fibs(20));
