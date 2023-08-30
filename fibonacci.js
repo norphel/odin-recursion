ed8a352c410bef1fa16409aeb3039daa3b5d2d8d
norphel <norphel317@gmail.com>
2023-08-30 11:29:23 +0530

    add recursive approach

--- fibonacci.js
+++ fibonacci.js
@@ -7,4 +7,15 @@ function fibs(n) {
   }
   return arr;
 }
-console.log(fibs(20));
+
+function fibsRec(n) {
+  if (n === 2) {
+    return [0, 1];
+  } else if (n <= 1) {
+    return [0];
+  } else {
+    return fibsRec(n - 1).concat([
+      fibsRec(n - 1)[n - 3] + fibsRec(n - 1)[n - 2],
+    ]);
+  }
+}
