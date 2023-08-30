1bdd963bf591238319d4a5d83d592419a063b07b
norphel <norphel317@gmail.com>
2023-08-30 13:48:41 +0530

    implement merge sort algorithm

--- mergeSort.js
+++ mergeSort.js
@@ -1,4 +1,12 @@
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  } else {
+    const leftHalf = arr.slice(0, parseInt(arr.length / 2));
+    const rightHalf = arr.slice(parseInt(arr.length / 2), arr.length);
+    return merge(mergeSort(leftHalf), mergeSort(rightHalf));
+  }
+}
 
 function merge(arr1, arr2) {
   let sortedArray = [];
@@ -20,3 +28,5 @@ function merge(arr1, arr2) {
   }
   return sortedArray;
 }
+
+console.log(mergeSort([20, 4, 1, 8, 3, 2, 9, 12, 10, 0, 4])); //[0, 1, 2, 3, 4, 4, 8, 9, 10, 12, 20]
